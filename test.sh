3b28b5dab74cd971fdcab32bf83e66ec3fd31321
Mauricio Prado <mauricioprado00@gmail.com>
2020-03-08 18:27:20 +0100

    Rename container

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 target="$1"
 
-docker-compose run --rm -w /app app \
+docker-compose run --rm -w /app phpunit \
     ./vendor/bin/phpunit \
         --bootstrap vendor/autoload.php \
         "${@:2}"  ${target}
